30cc49c96e325c1cb1bf2e2f9399cc3aedd73432
llgxvi <58665866+llgxvi@users.noreply.github.com>
2021-02-21 15:15:22 +0800

    Create certbot.sh

---
+++ certbot.sh
@@ -0,0 +1,9 @@
+apt install certbot
+
+certbot --standalone -d x.com certonly
+
+A=/etc/letsencrypt/live/x.com
+B=/usr/local/nginx/conf
+
+ln -s $A/fullchain.pem $B/x.com.crt
+ln -s $A/privkey.pem   $B/x.com.key
